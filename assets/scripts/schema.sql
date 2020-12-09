27018ec61ebfff172c54759228123e53b3aaa4a8
marandaz <mrzwies@gmail.com>
2020-12-08 22:30:49 -0600

    adding in schema and db set up

---
+++ assets/scripts/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employeeDB;
+CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  role_id INT NOT NULL,
+  manager_id INT default NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(role_id) REFERENCES role(id),
+  FOREIGN KEY(manager_id) REFERENCES employee(id)
+);
+
+CREATE TABLE department (
+  id INT AUTO_INCREMENT NOT NULL,
+  dept_name VARCHAR(50),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT NOT NULL,
+  title VARCHAR(50),
+  salary INT NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(department_id) REFERENCES department(id)
+);
